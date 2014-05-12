30bbc5faf83e9ae95c358aff6b3d30808ea5c28d
Thomas Recouvreux <thomas.recouvreux@polyconseil.fr>
2014-05-12 18:29:46 +0200

    tests: Move frame test out of encoder test

--- aioamqp/tests/test_frame.py
+++ aioamqp/tests/test_frame.py
@@ -38,23 +38,6 @@ class EncoderTestCase(unittest.TestCase):
             (b'F\x00\x00\x00\x18\x03barS\x00\x00\x00\x03baz\x03fooS\x00\x00\x00\x03bar',
              b'F\x00\x00\x00\x18\x03fooS\x00\x00\x00\x03bar\x03barS\x00\x00\x00\x03baz'))
 
-    def test_dump_dont_crash(self):
-        frame = AmqpResponse(None)
-        frame.frame_type = amqp_constants.TYPE_METHOD
-        self.class_id = 0
-        self.method_id = 0
-        saved_stout = sys.stdout
-        frame_module.DUMP_FRAMES = True
-        sys.stdout = io.StringIO()
-        try:
-            last_len = len(sys.stdout.getvalue())
-            frame.frame()
-            # assert something has been writen
-            self.assertLess(last_len, len(sys.stdout.getvalue()))
-        finally:
-            frame_module.DUMP_FRAMES = False
-            sys.stdout = saved_stout
-
     def test_write_message_properties_dont_crash(self):
         properties = {
             'content_type': 'plain/text',
@@ -81,3 +64,22 @@ class EncoderTestCase(unittest.TestCase):
         }
         with self.assertRaises(ValueError):
             self.encoder.write_message_properties(properties)
+
+
+class AmqpResponseTestCase(unittest.TestCase):
+    def test_dump_dont_crash(self):
+        frame = AmqpResponse(None)
+        frame.frame_type = amqp_constants.TYPE_METHOD
+        self.class_id = 0
+        self.method_id = 0
+        saved_stout = sys.stdout
+        frame_module.DUMP_FRAMES = True
+        sys.stdout = io.StringIO()
+        try:
+            last_len = len(sys.stdout.getvalue())
+            frame.frame()
+            # assert something has been writen
+            self.assertLess(last_len, len(sys.stdout.getvalue()))
+        finally:
+            frame_module.DUMP_FRAMES = False
+            sys.stdout = saved_stout
