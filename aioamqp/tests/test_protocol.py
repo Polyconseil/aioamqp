8d345b71e1b723e08edb070e007fcaf07e1a247e
Thomas Recouvreux <thomas.recouvreux@polyconseil.fr>
2014-05-13 16:21:34 +0200

    more test coverage

--- aioamqp/tests/test_protocol.py
+++ aioamqp/tests/test_protocol.py
@@ -4,10 +4,12 @@
 
 import asyncio
 import unittest
-import unittest.mock
+from unittest import mock
 
 from .. import exceptions
 from .. import connect as amqp_connect
+from .. import from_url as amqp_from_url
+from .. import protocol
 
 
 class ProtocolTestCase(unittest.TestCase):
@@ -34,3 +36,25 @@ class ProtocolTestCase(unittest.TestCase):
     def test_connection_wrong_login_password(self):
         with self.assertRaises(exceptions.AmqpClosedConnection):
             self.loop.run_until_complete(amqp_connect(login='wrong', password='wrong'))
+
+    def test_connection_from_url(self):
+        @asyncio.coroutine
+        def go():
+            with mock.patch('aioamqp.connect') as connect:
+                yield from amqp_from_url('amqp://tom:pass@chev.eu:7777/myvhost')
+                connect.assert_called_once_with(
+                    insist=False,
+                    password='pass',
+                    login_method='AMQPLAIN',
+                    ssl=False,
+                    login='tom',
+                    host='chev.eu',
+                    protocol_factory=protocol.AmqpProtocol,
+                    virtualhost='/myvhost',
+                    port=7777
+                )
+        self.loop.run_until_complete(go())
+
+    def test_from_url_raises_on_wrong_scheme(self):
+        with self.assertRaises(ValueError):
+            self.loop.run_until_complete(amqp_from_url('invalid://'))
