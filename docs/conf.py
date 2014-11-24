d276a48180e341d719bedf516a3594ddf6af8fb6
Cedric bache <cedric.bache@polyconseil.fr>
2014-11-24 17:55:18 +0100

    docs: fix documentation build

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'aioamqp'
-copyright = '2014, Benoît Calvez'
+copyright = u'2014, Benoît Calvez'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
