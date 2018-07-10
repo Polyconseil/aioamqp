735438a6120257aaa19222e0657bc07407721875
Loïs Burg <burg.lois@gmail.com>
2018-07-10 15:52:50 +0200

    setup.py: Add embedded classifier

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Embedded Systems",
     ],
     platforms='all',
     license='BSD'
